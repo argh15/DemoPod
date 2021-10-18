79273cb4db2577e5a706759699b52dad327cc9fc
Arghadeep <arghadeep@Arghadeeps-MacBook-Pro.local>
2021-10-18 13:39:37 +0530

    First commit

---
+++ DemoPod/Demo.swift
@@ -0,0 +1,15 @@
+//
+//  Demo.swift
+//  DemoPod
+//
+//  Created by Arghadeep on 18/10/21.
+//
+
+import Foundation
+
+public struct Demo {
+    
+    public static func hello() {
+        print("Hello World!")
+    }
+}
---
+++ DemoPod/DemoPod.h
@@ -0,0 +1,18 @@
+//
+//  DemoPod.h
+//  DemoPod
+//
+//  Created by Arghadeep on 18/10/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoPod.
+FOUNDATION_EXPORT double DemoPodVersionNumber;
+
+//! Project version string for DemoPod.
+FOUNDATION_EXPORT const unsigned char DemoPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoPod/PublicHeader.h>
+
+
